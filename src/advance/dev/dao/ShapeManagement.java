3edd034f42a0b5bbf0fc96614424252cd0ab2d44
dung9903 <dungnguyen9903@gmail.com>
2022-04-28 08:46:02 +0700

    first commit

---
+++ src/advance/dev/dao/ShapeManagement.java
@@ -0,0 +1,25 @@
+package advance.dev.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import advance.dev.models.Shape;
+
+public class ShapeManagement implements IShape {
+	private List<Shape> list = new ArrayList<Shape>();
+	
+	@Override
+	public void add(Shape shape) {
+		// TODO Auto-generated method stub
+		list.add(shape);
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		for (Shape shape : list) {
+			System.out.println(shape);
+		}
+	}
+
+}
